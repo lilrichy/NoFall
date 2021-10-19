93340414415b4fe8005cf364fa728cc4a1c20912
LiLRichy <richardreigens@gmail.com>
2021-10-19 19:09:34 -0400

    Initial Setup

--- src/main/java/com/lilrichy/nofall/NoFall.java
+++ src/main/java/com/lilrichy/nofall/NoFall.java
@@ -1,10 +1,10 @@
-package net.fabricmc.example;
+package com.lilrichy.nofall;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class NoFall implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -16,6 +16,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Help I've fallen and I can't get up!");
 	}
 }
