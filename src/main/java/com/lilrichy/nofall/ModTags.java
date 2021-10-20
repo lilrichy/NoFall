444a58c5003f062e41cff4c64c416bc43db0930f
LiLRichy <richardreigens@gmail.com>
2021-10-20 14:04:40 -0400

    Changed Tags to use .identified

--- src/main/java/com/lilrichy/nofall/ModTags.java
+++ src/main/java/com/lilrichy/nofall/ModTags.java
@@ -8,30 +8,30 @@ import net.minecraft.util.Identifier;
 
 public class ModTags {
     public static class Blocks {
-        public static final Tag<Block> CONCRETE_POWDER = createCommonTag("concrete_powder");
-        public static final Tag<Block> GRAVEL = createCommonTag("gravel");
-        public static final Tag<Block> SAND = createCommonTag("sand");
+        public static final Tag.Identified<Block> CONCRETE_POWDER = createCommonTag("concrete_powder");
+        public static final Tag.Identified<Block> GRAVEL = createCommonTag("gravel");
+        public static final Tag.Identified<Block> SAND = createCommonTag("sand");
 
-        private static Tag<Block> createTag(String name) {
+        private static Tag.Identified<Block> createTag(String name) {
             return TagFactory.BLOCK.create(new Identifier(NoFall.MOD_ID, name));
         }
 
-        private static Tag<Block> createCommonTag(String name) {
+        private static Tag.Identified<Block> createCommonTag(String name) {
             return TagFactory.BLOCK.create(new Identifier("c", name));
         }
 
     }
 
     public static class Items {
-        public static final Tag<Item> CONCRETE_POWDER = createCommonTag("concrete_powder");
-        public static final Tag<Item> GRAVEL = createCommonTag("gravel");
-        public static final Tag<Item> SAND = createCommonTag("sand");
+        public static final Tag.Identified<Item> CONCRETE_POWDER = createCommonTag("concrete_powder");
+        public static final Tag.Identified<Item> GRAVEL = createCommonTag("gravel");
+        public static final Tag.Identified<Item> SAND = createCommonTag("sand");
 
-        private static Tag<Item> createTag(String name) {
+        private static Tag.Identified<Item> createTag(String name) {
             return TagFactory.ITEM.create(new Identifier(NoFall.MOD_ID, name));
         }
 
-        private static Tag<Item> createCommonTag(String name) {
+        private static Tag.Identified<Item> createCommonTag(String name) {
             return TagFactory.ITEM.create(new Identifier("c", name));
         }
 
